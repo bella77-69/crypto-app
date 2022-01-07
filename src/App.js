e5eb5065f659d6770e47806c0484b11edfaafb2b
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-06 17:20:30 -0800

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.scss';
-
+import Header from './components/Header/Header'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
      hi
       </header>
     </div>
