3a1ac9f53cf73f7ddaa662acb149df70dfcdd6fd
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-06 17:16:35 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     hi
+      </header>
+    </div>
+  );
+}
+
+export default App;
